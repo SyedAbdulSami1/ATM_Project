9de6357c0ad79664b95bca1da37c523db2bf87e9
SyedAbdulSami1 <samiwpp@gmail.com>
2024-03-28 08:50:40 +0500

    coding for Enter Pin code

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let currentBalance = 10000;
+console.log(`Your current Balance : ${currentBalance}`);
+let pCode = 1234;
+let received_pcode = await inquirer.prompt({
+    name: "answer",
+    message: "Please Enter Your correct Pin Code",
+    type: "number"
+});
+if (received_pcode.answer === pCode) {
+    console.log(`Your Pin Code in Correct`);
+    let operation = await inquirer.prompt({
+        name: "selectedOperator",
+        message: "What do you want?",
+        type: "list",
+        choices: ["Withdraw", "Balance"]
+    });
+}
--- index.ts
+++ index.ts
@@ -2,4 +2,27 @@
 
 import inquirer from "inquirer";
 
-let opration = awiat inquirer.prompt ()
+let currentBalance : number = 10000;
+console.log(`Your current Balance : ${currentBalance}`);
+let pCode : number = 1234 ;
+
+
+let received_pcode = await inquirer.prompt(
+    {
+        name: "answer",
+        message: "Please Enter Your correct Pin Code",
+        type: "number"
+    }
+);
+
+if (received_pcode.answer===pCode) {
+    console.log(`Your Pin Code in Correct`);
+    let operation = await inquirer.prompt(
+        {
+            name:"selectedOperator",
+            message: "What do you want?",
+            type: "list",
+            choices:["Withdraw", "Balance"]
+        }
+    )
+}
