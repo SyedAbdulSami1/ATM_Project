61995b0fb88a84e9e8a59ee0a0bf9f3507d0f718
SyedAbdulSami1 <samiwpp@gmail.com>
2024-03-29 06:26:54 +0500

    Now manual Amount input is working but not given error on type above amount

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import inquirer from "inquirer";
 let currentBalance = 10000;
 console.log(`Your current Balance : ${currentBalance}`);
 let pCode = 1234;
-let received_pcode = await inquirer.prompt({
+let received_pwsCode = await inquirer.prompt({
     name: "answer",
     message: "Please Enter Your correct Pin Code:",
     type: "number"
 });
-if (received_pcode.answer === pCode) {
+if (received_pwsCode.answer === pCode) {
     console.log(`Your Pin Code in Correct`);
     let operation = await inquirer.prompt({
         name: "selectedOperator",
@@ -23,14 +23,15 @@ if (received_pcode.answer === pCode) {
             type: "list",
             choices: ["1000", "3000", "5000", "10000", "20000", "otherAmount"],
         });
-        if (afterCurrentBalance.nrAmount != "otherAmount" && afterCurrentBalance.nrAmount <= currentBalance) {
+        let selectedAmount = afterCurrentBalance.nrAmount;
+        if (selectedAmount != "otherAmount" && selectedAmount <= currentBalance) {
             console.log(`Now your balance is: ${currentBalance - afterCurrentBalance.nrAmount}`);
         }
-        else if (afterCurrentBalance.nrAmount == "otherAmount" && afterCurrentBalance.nrAmount <= currentBalance) {
+        else if (selectedAmount === "otherAmount") {
             let typeAmount = await inquirer.prompt({
                 name: "amountByType",
-                message: "Enter your Amount",
-                type: "number"
+                message: "Enter your Amount:",
+                type: "number",
             });
             console.log(`Now your balance is: ${currentBalance - typeAmount.amountByType}`);
         }
